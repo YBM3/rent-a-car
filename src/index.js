6cbd4a21072591e74eaa92113b93ab4cd7461a41
YBM3 <you@example.com>
2023-12-25 01:28:23 +0100

    fix index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import App from './components/App/App';
+
 import { store, persistor } from './redux/store';
 
 import './index.css';
 import './fonts/fonts';
+import App from 'components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
