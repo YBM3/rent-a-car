0529f4144c6b99cf9069c6c00db4d013056ec739
YBM3 <you@example.com>
2023-12-25 01:43:56 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/rent-a-car">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
